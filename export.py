8772b81c24b36b1dc28370d43d5f8a95f28a022b
Mowd <mowd@itri.org.tw>
2016-02-15 17:10:56 +0800

    Initial commit.

---
+++ export.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.6
+# -*- encoding: utf8 -*-
+
+import sys
+import optparse
+import urllib2
+import json
+import codecs
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+def main(args):
+    '''\
+    %prog [options]
+    '''
+    response = urllib2.urlopen('https://wapi.gogoro.com/tw/api/vm/list')
+    data = json.load(response)
+    with codecs.open("gostation.csv", "w", "utf-8") as fo:
+        fo.write(u"站名,地址,營業時間,目前狀態,緯度,經度\r\n")
+        for d in data["data"]:
+            locname = json.loads(d["LocName"])
+            address = json.loads(d["Address"])
+            fo.write(u"%s,%s,%s,%s,%f,%f\r\n" %
+            (locname["List"][1]["Value"],
+             address["List"][1]["Value"],
+             d["AvailableTime"],
+             d["State"],
+             d["Latitude"],
+             d["Longitude"]
+            ))
+    return 0
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage=main.__doc__)
+    options, args = parser.parse_args()
+
+    if len(args) != 0:
+        parser.print_help()
+        sys.exit(1)
+
+    sys.exit(main(args))
+
