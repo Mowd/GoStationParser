087f3cd577701f8ceb4164651eef5d71e4b7cd69
Mowd <mowd8574@gmail.com>
2016-05-17 18:34:28 +0800

    Sort gostation result before add to csv.

--- export.py
+++ export.py
@@ -15,30 +15,36 @@ def main(args):
     '''\
     %prog [options]
     '''
+    lines = []
     current_path = os.path.dirname(os.path.abspath(__file__))
     response = urllib2.urlopen('https://wapi.gogoro.com/tw/api/vm/list')
     data = json.load(response)
-    with codecs.open("%s/gostation-%s.csv"
-                     % (current_path, datetime.now().strftime("%Y%m%d")),
-                     "w", "utf-8") as fo:
-        fo.write(u"站名,地址,營業時間,目前狀態,緯度,經度\r\n")
-        for d in data["data"]:
-            locname = json.loads(d["LocName"])
-            address = json.loads(d["Address"])
-            if d["State"] == 1:
-                state = u"已啟用"
-            elif d["State"] == 99:
-                state = u"建置中"
-            else:
-                state = u"建置中(其他狀態%s)" % d["State"]
-            fo.write(u"%s,%s,%s,%s,%f,%f\r\n" %
-            (locname["List"][1]["Value"],
-             address["List"][1]["Value"],
+    for d in data["data"]:
+        locname = json.loads(d["LocName"])
+        address = json.loads(d["Address"])
+        address = address["List"][1]["Value"].replace(",", u"，")
+        if d["State"] == 1:
+            state = u"已啟用"
+        elif d["State"] == 99:
+            state = u"建置中"
+        else:
+            state = u"建置中(其他狀態%s)" % d["State"]
+        lines.append(u"%s,%s,%s,%s,%f,%f" %
+            (
+             locname["List"][1]["Value"],
+             address,
              d["AvailableTime"],
              state,
              d["Latitude"],
              d["Longitude"]
-            ))
+            )
+        )
+    lines = sorted(lines)
+    with codecs.open("%s/gostation-%s.csv"
+                     % (current_path, datetime.now().strftime("%Y%m%d")),
+                     "w", "utf-8") as fo:
+        fo.write(u"站名,地址,營業時間,目前狀態,緯度,經度\r\n")
+        fo.write("\r\n".join(lines))
     return 0
 
 
